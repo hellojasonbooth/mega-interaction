a890aa0a8c8e7b62c4b422e53726775a2f41f79e
Jason Booth <hello@studiojasonbooth.com>
2020-02-25 11:17:30 +0000

    added seventh interaction

---
+++ dist/scripts/test-seven.js
@@ -0,0 +1,81 @@
+const canvasTag = document.querySelector("section canvas")
+
+canvasTag.width = window.innerWidth * 2
+canvasTag.height = window.innerHeight * 2
+
+canvasTag.style.width = window.innerWidth + "px"
+canvasTag.style.height = window.innerHeight + "px"
+
+const context = canvasTag.getContext("2d")
+context.scale(2, 2)
+
+let aimX = null
+let aimY = null
+let currentX = null
+let currentY = null
+
+let positions = []
+
+let i = 0
+const images = ["img/letter0.png", "img/letter1.png", "img/letter2.png", "img/letter3.png"].map(src => {
+  const image = document.createElement("img")
+  image.src = "/" + src
+  return image
+})
+
+document.addEventListener("mousemove", function (event) {
+  aimX = event.pageX
+  aimY = event.pageY
+  if (currentX === null) {
+    currentX = event.pageX
+    currentY = event.pageY
+  }
+
+})
+
+// canvasTag.addEventListener("click", function () {
+//   i = i + 1
+//   if (i >= images.length) {
+//     i = 0
+//   }
+// })
+
+
+const draw = function () {
+
+    if (currentX) {    
+        currentX = currentX + (aimX - currentX) * 0.1
+        currentY = currentY + (aimY - currentY) * 0.1
+
+    // work out the distance between current place and aim
+    const dist = Math.sqrt(Math.pow(currentX - aimX, 2) + Math.pow(currentY - aimY, 2))
+
+    // if enough distance, add it the list
+    if (dist > 0.01) {
+        // save as what image now, x and y position as array
+        positions.push([images[i], currentX, currentY])
+    }
+
+    }
+  
+    // only the last 100
+    positions = positions.slice(-50)
+    
+    // remove EVERYTHING
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    
+    // and redraw based on the last 50 positions
+    positions.forEach(pos => {
+        context.drawImage(pos[0], pos[1] - 200, pos[2] - 300, images[i].width, images[i].height)
+    })
+    
+    requestAnimationFrame(draw)
+}
+
+draw()
+
+
+
+
+
+
