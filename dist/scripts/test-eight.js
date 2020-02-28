0ee3a090d9a45028d5624c0eaa6bf7f3a661cfe5
Jason Booth <hello@studiojasonbooth.com>
2020-02-28 10:50:03 +0000

    mobile image sizes in js added

--- dist/scripts/test-eight.js
+++ dist/scripts/test-eight.js
@@ -44,19 +44,13 @@ document.addEventListener("touchmove", function (event) {
   })
 
 
-
-
-
-
-
-
-    if ("ontouchstart" in document.documentElement) {
+    // if ("ontouchstart" in document.documentElement) {
       // if on mobile or tablet
-        setInterval(() => {
-            moveImage()
-        }, 2000)
+        // setInterval(() => {
+        //     moveImage()
+        // }, 2000)
         
-    } else {
+    // } else {
       // if on desktop
 
       setInterval(() => {
@@ -66,7 +60,7 @@ document.addEventListener("touchmove", function (event) {
             }
         }, 2000)
 
-      function makeNewPosition(){
+      function makeNewPosition() {
     
         // Get viewport dimensions (remove the dimension of the image)
         const w = window.innerWidth - 406
@@ -78,7 +72,6 @@ document.addEventListener("touchmove", function (event) {
         return [nw, nh]
      
     }
-    
     
     function moveImage() {
     
@@ -93,38 +86,46 @@ document.addEventListener("touchmove", function (event) {
     
     moveImage()
 
+    const draw = function () {
 
-      const draw = function () {
-
-        if (currentX) {    
-            currentX = currentX + (aimX - currentX) * 0.1
-            currentY = currentY + (aimY - currentY) * 0.1
-    
-        // work out the distance between current place and aim
-        const dist = Math.sqrt(Math.pow(currentX - aimX, 2) + Math.pow(currentY - aimY, 2))
-    
-            // if enough distance, add it the list
-            if (dist > 40) {
-                // save current images, x and y position as array
-                positions.push([images[i], currentX, currentY])
-            }
-        
-        }
+      if (currentX) {    
+          currentX = currentX + (aimX - currentX) * 0.1
+          currentY = currentY + (aimY - currentY) * 0.1
+  
+      // work out the distance between current place and aim
+      const dist = Math.sqrt(Math.pow(currentX - aimX, 2) + Math.pow(currentY - aimY, 2))
+  
+          // if enough distance, add it the list
+          if (dist > 40) {
+              // save current images, x and y position as array
+              positions.push([images[i], currentX, currentY])
+          }
       
-        // only the last 100
-        positions = positions.slice(-60)
-        
-        // remove EVERYTHING
-        context.clearRect(0, 0, window.innerWidth, window.innerHeight)
-    
+      }
+      
+      // only the last 100
+      positions = positions.slice(-60)
+      
+      // remove EVERYTHING
+      context.clearRect(0, 0, window.innerWidth, window.innerHeight)
+
+      // adjust the image size depending on screen sizes
+      // then draw images
+      if(window.innerWidth > 900) {
         positions.forEach((pos, index) => {
           context.globalAlpha = index / 10
           context.drawImage(pos[0], pos[1] - 200, pos[2] - 200, 406, 436)
         })
-        
-        requestAnimationFrame(draw)
-    }
-    
-    draw()
+      } else {
+        positions.forEach((pos, index) => {
+          context.globalAlpha = index / 10
+          context.drawImage(pos[0], pos[1] - 200, pos[2] - 200, 203, 218)
+        })
 
+      }
+      
+      requestAnimationFrame(draw)
     }
+   
+    // call draw function on load
+    draw()
